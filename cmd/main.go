0b5fdd900b81397a405a21792d73e3f446a1cc96
Mike <michael.johanson@intel.com>
2022-04-27 10:48:15 -0700

    feat(env): add option to override default mps host
    closes: Container name of MPS is hardcoded #27

--- cmd/main.go
+++ cmd/main.go
@@ -27,16 +27,21 @@ func main() {
 
 	routerPort := os.Getenv("PORT")
 	if routerPort == "" {
-		log.Println("PORT env is not set")
+		log.Println("PORT env is not set, default is 8003")
 		routerPort = "8003"
 	}
 	mpsPort := os.Getenv("MPS_PORT")
 	if mpsPort == "" {
-		log.Println("MPS_PORT env is not set")
+		log.Println("MPS_PORT env is not set, default is 3000")
 		mpsPort = "3000"
 	}
+	mpsHost := os.Getenv("MPS_HOST")
+	if mpsHost == "" {
+		log.Println("MPS_HOST env is not set,default is mps")
+		mpsHost = "mps"
+	}
 
-	p := proxy.NewServer(":"+routerPort, "mps:"+mpsPort)
+	p := proxy.NewServer(":"+routerPort, mpsHost+":"+mpsPort)
 	log.Println("Proxying from " + p.Addr + " to :" + p.Target)
 	p.ListenAndServe()
 }
